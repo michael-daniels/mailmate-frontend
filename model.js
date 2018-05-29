a7b2f46ab4bfff2963fdc2bbafaef54dea73ade0
‘Michael <michael.daniels.jr@gmail.com>
2018-05-28 19:49:09 -0700

    saving changes

--- model.js
+++ model.js
@@ -3,6 +3,7 @@ document.getElementById('tab1').style.display = 'block'
     el: '#app',
     data: {
       loadedContacts: [],
+      loadedDocuments: [{title:'Collection Notice', content:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."}],
       newContact: {
         contactName: '',
         streetAddress: '',
@@ -11,8 +12,13 @@ document.getElementById('tab1').style.display = 'block'
         state: '',
         zip: '',
       },
+      newDocument: {
+        title:'',
+        content:''
+      },
       selectedContacts: ['all selected contacts populated here, multiple objects'],
       selectedDocument: {
+        id:'',
         title:'',
         content:'',
       }
@@ -41,6 +47,23 @@ document.getElementById('tab1').style.display = 'block'
       addNewContact: (event) => {
         event.preventDefault()
         app.loadedContacts.unshift(app.newContact)
+        app.newContact = {
+          contactName: '',
+          streetAddress: '',
+          street2: '',
+          city: '',
+          state: '',
+          zip: ''
+        }
+
+      },
+      addNewDocument: (event) => {
+        event.preventDefault()
+        app.loadedDocuments.unshift(app.newDocument)
+        app.newDocument = {
+          title:'',
+          content:''
+        }
       },
       contactClicked: (event) => {
         alert(event.target.class)
